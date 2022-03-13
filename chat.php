721ad57abeb0f0c96ec14d25fc891e00169e1420
Fumiya Houmitsu <fumiya.houmitsu1028@gmail.com>
2022-03-13 22:56:36 +0900

    create db

--- chat.php
+++ chat.php
@@ -12,7 +12,16 @@
 </form>
 
 <?php
-print $_GET["a"];
+if(isset($_GET["a"])){
+$f=fopen("chat.txt","at");
+$mozi = htmlspecialchars($_GET["a"],ENT_QUOTES);
+fwrite($f,$mozi."\n");
+fclose($f);
+}
+
+print "<pre>";
+readfile("chat.txt");
+print "</pre>";
 ?>
 
 </body>
