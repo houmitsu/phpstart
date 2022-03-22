c262c193199c20b4395b0f72aa577f1fd052e94d
Fumiya Houmitsu <fumiya.houmitsu1028@gmail.com>
2022-03-22 23:44:17 +0900

    finish

--- sns3.php
+++ sns3.php
@@ -5,7 +5,18 @@
 <title>SNS3</title>
 </head>
 <body>
+<?php
+$my_sea=htmlspecialchars($_POST["s"], ENT_QUOTES);
+$db = new PDO("mysql:host=localhost;dbname=db","root","root");
 
+print "<p style='font-size:20pt'>「{$my_sea}」の検索結果</p>";
+$ps=$db->query("SELECT * FROM tb WHERE mes like '%$my_sea%'");
+while ($r = $ps->fetch()){
+      print "{$r['ban']} {$r['nam']} {$r['dat']}<br>"
+            .nl2br($r['mes'])."<hr>";
+}
+print "<p><a href='sns1.php'>一覧表示へ</a></p>";
+?>
 
 
 </body>
